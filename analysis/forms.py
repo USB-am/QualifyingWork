c7526280de897ac8efa9e77250c6de9aa3e91b5f
Цупко Роман Игоревич <smit.acer@mail.ru>
2022-11-28 16:48:22 +0300

    Created analysis template

--- analysis/forms.py
+++ analysis/forms.py
@@ -4,4 +4,4 @@ from django import forms
 class AnalysisForm(forms.Form):
 	''' Форма ввода url для анализа '''
 
-	url = forms.CharField()
+	url = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'URL...'}))
