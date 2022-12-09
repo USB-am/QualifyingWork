6a4bad015e5abfc278fea2181edcf07885db34bc
Цупко Роман Игоревич <smit.acer@mail.ru>
2022-12-09 16:52:18 +0300

    Added content text parser

--- logics/parsers/text.py
+++ logics/parsers/text.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from typing import Union
 import re
 
 import requests
@@ -61,3 +62,56 @@ def pars_links(soup: bs) -> list:
 		output.append(LinkTag(content=text, href=href))
 
 	return output
+
+
+@dataclass
+class Text():
+	texts: Union[list, tuple]
+
+	def __str__(self):
+		return self.text
+
+	@property
+	def text(self) -> str:
+		return '\n'.join(self.texts)
+
+	@property
+	def length(self) -> int:
+		return len(self.text)
+
+	def first_text_percent(self, limit: int) -> str:
+		limit %= 100	# Max percent = 100%
+		index_from_percent = self.length // limit
+		sliced_text = self.text[:index_from_percent]
+
+		return sliced_text
+
+
+def pars_all_text(soup: bs) -> str:
+	text_tags = pars_text(soup)
+	p_tags = map(
+		lambda tag: tag.content,
+		filter(
+			lambda tag: tag.name == 'p',
+			text_tags
+		)
+	)
+
+	return p_tags
+
+
+def main():
+	import requests
+	from bs4 import BeautifulSoup as bs
+
+	url = 'https://habr.com/ru/post/704440/'
+	html = requests.get(url).text
+	soup = bs(html, 'html.parser')
+	all_texts = pars_all_text(soup)
+	text = Text(texts=tuple(all_texts))
+
+	print(text.first_text_percent(20))
+
+
+if __name__ == '__main__':
+	main()
