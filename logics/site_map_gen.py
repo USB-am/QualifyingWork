1cd8ced2b3a9ea590b71165bfdca3b03f272a57a
USB-am <smit.acer@mail.ru>
2022-12-26 02:20:00 +0300

    Added tree structure

--- logics/site_map_gen.py
+++ logics/site_map_gen.py
@@ -1,18 +1,19 @@
 # -*- coding: utf-8 -*-
 
 from urllib.parse import urlparse
+from dataclasses import dataclass
 
 import requests
 from bs4 import BeautifulSoup as bs
 from bs4 import element
 
 
-URL = 'https://habr.com/ru/post/240463/'
-DOMAIN = urlparse(URL).netloc
+_URL = 'https://habr.com/ru/post/240463/'
+DOMAIN = urlparse(_URL).netloc
 headers = {
 	'Content-Type': 'text/html; charset=utf-8',
 	'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 '
-		'(KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36'
+		'(KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36',
 	'Accept-Language': 'ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7',
 }
 
@@ -65,24 +66,7 @@ class Parser:
 
 
 class SiteMap(dict):
-	'''
-	Inited site map. Has structure:
-	{
-		domain: [
-			{
-				'app_1': [
-					{
-						'app_1_1': [...]
-					},
-					...
-				],
-				...
-			},
-			...
-		]
-	}
-
-	'''
+	''' Inited site map '''
 
 	def __init__(self, domain: str):
 		super().__init__()
@@ -102,8 +86,50 @@ class SiteMap(dict):
 		return True
 
 
+class URL:
+	def __init__(self, url: str):
+		self.url = url.split(DOMAIN)[1]
+		self.apps = self.url.split('/')[1:-1]
+
+	def __str__(self):
+		return self.url
+
+
+class Node:
+	''' Элемент дерева '''
+
+	def __init__(self, app_name: str, childrens: list=[]):
+		self.app_name = app_name
+		self.childrens = childrens
+
+	def __in__(self, other: str) -> bool:
+		return True
+		# TODO: сделать поиск по дочерним элементам
+
+
+class Tree(dict):
+	''' Предсталение дерева приложений сайта '''
+
+	def __init__(self, parent: Node):
+		super().__init__()
+
+		self[DOMAIN] = parent
+
+	def add(self, url: URL) -> None:
+		current_node = self[DOMAIN]
+
+		for app in url.apps:
+			for node in current_node.childrens:
+				if app == node.app_name:
+					print(f'{app} is exists')
+				else:
+					print(f'{app} is not found')
+
+
 if __name__ == '__main__':
 	site_map = SiteMap(f'https://{DOMAIN}')
 
-	for link in site_map.links:
-		print(link)
+	# for link in site_map.links:
+	# 	print(link)
+	tree = Tree(Node(app_name=DOMAIN))
+	tree.add(URL('https://habr.com/ru/post/683956/'))
