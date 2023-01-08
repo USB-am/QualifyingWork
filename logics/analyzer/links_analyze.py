4a7b76870488e924e233b563f3e8df2dfd3ed572
USB-am <smit.acer@mail.ru>
2023-01-08 15:09:13 +0300

    Split functions to packages

---
+++ logics/analyzer/links_analyze.py
@@ -0,0 +1,90 @@
+from dataclasses import dataclass
+
+from bs4 import element
+
+
+def delete_internal_links(func):
+	def wrapper(*args, **kwargs):
+		links = func(*args, **kwargs)
+		for link in links:
+			link.href = link.href.split('#')[0]
+
+		return links
+	return wrapper
+
+
+def delete_options(func):
+	def wrapper(*args, **kwargs):
+		links = func(*args, **kwargs)
+		for link in links:
+			link.href = link.href.split('?')[0]
+
+		return links
+	return wrapper
+
+
+def delete_empty_link(func):
+	def wrapper(*args, **kwargs):
+		links = func(*args, **kwargs)
+		for link in links:
+			if len(link.href) == 0:
+				links.remove(link)
+
+		return links
+	return wrapper
+
+
+def delete_duplicate(func):
+	def wrapper(*args, **kwargs):
+		links = func(*args, **kwargs)
+		hrefs = []
+
+		for link in links:
+			href = link.href
+			if href in hrefs:
+				links.remove(link)
+			else:
+				hrefs.append(href)
+
+		return links
+	return wrapper
+
+
+@dataclass
+class Link:
+	name = 'a'
+	text: str
+	href: str
+
+	def __str__(self):
+		return str(self.href)
+
+
+@delete_duplicate
+@delete_empty_link
+@delete_options
+@delete_internal_links
+def normalize_links(domain: str, links: element.ResultSet) -> list:
+	output = []
+
+	for link in links:
+		href = link.attrs.get('href')
+		if href is None:
+			continue
+
+		if href[0] == '/':
+			href = domain + href
+		text = link.text.strip()
+
+		output.append(Link(text, href))
+
+	return output
+
+
+def links_analyzer(domain: str, links: list) -> None:
+	links = normalize_links(domain, links)
+	for link in links:
+		# text = link.text.strip()
+		# href = link.attrs.get('href')
+		# print(f'{text} [{href}]')
+		print(f'"{link}"')
--- logics/parser.py
+++ logics/parser.py
@@ -5,6 +5,8 @@ from bs4 import BeautifulSoup as bs
 from bs4 import element
 from fuzzywuzzy import fuzz
 
+from analyzer import links_analyze
+
 
 def get_html(url: str) -> str:
 	return requests.get(url).text
@@ -48,88 +50,6 @@ headers = {
 # === Analyzer === #
 def title_to_h1(title_text: str, h1_text: str) -> int:
 	return fuzz.partial_ratio(title_text, h1_text)
-
-
-class Link:
-	name = 'a'
-
-	def __init__(self, text: str, href: str):
-		self.text = text
-		self.href = href
-
-	def __str__(self):
-		return str(self.href)
-
-
-def delete_internal_links(func):
-	def wrapper(*args, **kwargs):
-		links = func(*args, **kwargs)
-		for link in links:
-			link.href = link.href.split('#')[0]
-
-		return links
-	return wrapper
-
-def delete_options(func):
-	def wrapper(*args, **kwargs):
-		links = func(*args, **kwargs)
-		for link in links:
-			link.href = link.href.split('?')[0]
-
-		return links
-	return wrapper
-
-def delete_empty_link(func):
-	def wrapper(*args, **kwargs):
-		links = func(*args, **kwargs)
-		for link in links:
-			if len(link.href) == 0:
-				links.remove(link)
-
-		return links
-	return wrapper
-
-def delete_duplicate(func):
-	def wrapper(*args, **kwargs):
-		links = func(*args, **kwargs)
-		hrefs = []
-		for link in links:
-			href = link.href
-			if href in hrefs:
-				links.remove(link)
-			else:
-				hrefs.append(href)
-		return links
-	return wrapper
-
-
-@delete_duplicate
-@delete_empty_link
-@delete_options
-@delete_internal_links
-def normalize_links(domain: str, links: element.ResultSet) -> list:
-	output = []
-
-	for link in links:
-		href = link.attrs.get('href')
-		if href is None:
-			continue
-
-		if href[0] == '/':
-			href = domain + href
-		text = link.text.strip()
-
-		output.append(Link(text, href))
-
-	return output
-
-def links_analyzer(domain: str, links: list) -> None:
-	links = normalize_links(domain, links)
-	for link in links:
-		# text = link.text.strip()
-		# href = link.attrs.get('href')
-		# print(f'{text} [{href}]')
-		print(f'"{link}"')
 # === Analyzer === #
 
 
@@ -140,4 +60,4 @@ if __name__ == '__main__':
 	# title = page.getElementByTag('title')[0]
 	# print(title_to_h1(title.text, h1.text))
 
-	links_analyzer(page.domain, page.getElementByTag('a'))
+	links_analyze.links_analyzer(page.domain, page.getElementByTag('a'))
