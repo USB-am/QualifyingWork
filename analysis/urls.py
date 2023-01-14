8ae31d81384a329a109e8b88c745e7cab25926a9
USB-am <smit.acer@mail.ru>
2023-01-14 22:43:39 +0300

    Added branches for site and page analysis

--- analysis/urls.py
+++ analysis/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
-from .views import AnalysisView, AnalysisInfoView
+from .views import AnalysisSiteView, AnalysisPageView, AnalysisSiteInfoView, AnalysisPageInfoView
 
 
 urlpatterns = [
-	path('', AnalysisView.as_view(), name='analysis'),
-	path('<str:scheme>/<str:url>/', AnalysisInfoView.as_view(), name='analysis_info')
+	path('site/', AnalysisSiteView.as_view(), name='site_analysis'),
+	path('site/<str:scheme>/<str:url>/', AnalysisSiteInfoView.as_view(), name='analysis_site_info'),
+	path('page/', AnalysisPageView.as_view(), name='page_analysis'),
+	path('page/<str:scheme>/<str:url>/', AnalysisPageInfoView.as_view(), name='analysis_page_info'),
 ]
--- analysis/views.py
+++ analysis/views.py
@@ -6,8 +6,6 @@ from django.views.generic import FormView, TemplateView
 from django.urls import reverse_lazy
 
 from .forms import AnalysisForm
-# from logics.analyzer import Analyzer, get_domain
-# from logics.analyzer.links_analyze import LinksAnalyzer
 from logics import Site
 
 
@@ -19,7 +17,30 @@ def get_domain(url: str) -> str:
 	return (scheme, domain)
 
 
-class AnalysisView(FormView):
+class AnalysisSiteView(FormView):
+	''' Базовое представление страцы с анализом '''
+
+	template_name = 'analysis/analysis.html'
+	form_class = AnalysisForm
+
+	def get_context_data(self, **kwargs):
+		context = super().get_context_data(**kwargs)
+
+		context['title'] = 'Analyze a web site - USBAM'
+
+		return context
+
+	def get_success_url(self):
+		url_input = self.request.POST.get('url')
+		scheme, url = get_domain(url_input)
+
+		return reverse_lazy(
+			viewname='analysis_site_info',
+			kwargs={'scheme': scheme, 'url': url}
+		)
+
+
+class AnalysisPageView(FormView):
 	''' Базовое представление страцы с анализом '''
 
 	template_name = 'analysis/analysis.html'
@@ -37,7 +58,7 @@ class AnalysisView(FormView):
 		scheme, url = get_domain(url_input)
 
 		return reverse_lazy(
-			viewname='analysis_info',
+			viewname='analysis_page_info',
 			kwargs={'scheme': scheme, 'url': url}
 		)
 
@@ -46,15 +67,31 @@ def input_to_domain(scheme: str, netloc: str) -> str:
 	return f'{scheme}://{netloc}'
 
 
-class AnalysisInfoView(TemplateView):
-	''' Базовое представление страницы с информацией '''
+class AnalysisSiteInfoView(TemplateView):
+	''' Базовое представление страницы с информацией сайта '''
 
 	template_name = 'analysis/information.html'
 
 	def get_context_data(self, **kwargs):
 		context = super().get_context_data(**kwargs)
 
-		context['title'] = 'Analysis Info'
+		context['title'] = 'Analysis site Info'
+		context['site'] = Site(
+			input_to_domain(context['scheme'], context['url'])
+		)
+
+		return context
+
+
+class AnalysisPageInfoView(TemplateView):
+	''' Базовое представление страницы с информацией страницы '''
+
+	template_name = 'analysis/information.html'
+
+	def get_context_data(self, **kwargs):
+		context = super().get_context_data(**kwargs)
+
+		context['title'] = 'Analysis page Info'
 		context['site'] = Site(
 			input_to_domain(context['scheme'], context['url'])
 		)
