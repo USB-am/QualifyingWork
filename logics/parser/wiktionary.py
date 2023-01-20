4d5d68f5a6cd50daa0516097b13295c924fe339b
Цупко Роман Игоревич <smit.acer@mail.ru>
2023-01-20 16:21:40 +0300

    Fix exception and delete useless comments

--- logics/parser/wiktionary.py
+++ logics/parser/wiktionary.py
@@ -15,21 +15,6 @@ def get_html(url: str) -> str:
 	raise ValueError(f'Url "{url}" uncorrected!')
 
 
-# def get_synonyms_block(soup: bs) -> element.Tag:
-# 	'''
-# 	synonyms_title = soup.find('span', attrs={
-# 		'id': "Синонимы", 'class': 'mw-headline'
-# 	})
-# 	parent_block = synonyms_title.parent
-# 	synonyms_block = parent_block.next_sibling
-# 	print(dir(parent_block))
-# 	print(synonyms_block)
-# 	'''
-# 	a_tags = soup.find_all('a', title=True)
-# 	filtered_tags = filter(lambda a: a.text == a.attrs['title'], a_tags)
-# 	# [print(a.text) for a in filtered_tags]
-
-
 def get_synonyms(word: str) -> list:
 	url = URL.format(word=word)
 	html = get_html(url)
@@ -38,7 +23,6 @@ def get_synonyms(word: str) -> list:
 	if not ('синоним' in html.lower()):
 		return []
 
-	synonyms_block = get_synonyms_block(soup)
 	a_tags = soup.find_all('a', title=True)
 	filtered_tags = filter(lambda a: a.text == a.attrs['title'], a_tags)
 	synonyms = list(map(lambda tag: tag.text, filtered_tags))
