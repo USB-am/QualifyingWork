b3f0e0230fb20b8ab71ce555b3b0b85e5cbcc732
USB-am <smit.acer@mail.ru>
2023-01-22 23:44:27 +0300

    Created submodule for working with GoogleApi

---
+++ logics/parser/google_api.py
@@ -0,0 +1,13 @@
+API_KEY = ''
+TOKEN = ''
+
+
+def get_top_page(request: str) -> str:
+	'''
+	Получает лучший (первый) результат в выдаче поисковика
+	по принимаемому запросу
+	:params
+	~request: str - текст (запрос)
+	'''
+
+	return ''
