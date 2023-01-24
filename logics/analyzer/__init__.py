95011bd10b279a7f98273233369631e85491bfc3
Цупко Роман Игоревич <smit.acer@mail.ru>
2023-01-24 17:02:54 +0300

    Rewrited analyzer

---
+++ logics/analyzer/__init__.py
@@ -0,0 +1,25 @@
+from logics.tools import Request
+from logics import parser as Parser
+
+
+class Analyzer:
+	'''
+	Базовый класс, хранящий результаты анализа веб-страницы
+	:params
+	~request: Request - ссылка на веб-страницу
+	'''
+
+	def __init__(self, request: Request):
+		self.request = request
+
+	@property
+	def favicon(self) -> str:
+		return Parser.get_favicon(self.request)
+
+	@property
+	def desctiption(self) -> str:
+		return Parser.get_desctiption(self.request)
+
+	@property
+	def keywords(self) -> list:
+		return Parser.get_keywords(self.request)
