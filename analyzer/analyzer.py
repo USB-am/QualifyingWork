c6b16e3ef6800d57d67abdaa09d5c8354b6575c6
USB-am <smit.acer@mail.ru>
2023-02-06 01:05:08 +0300

    Finished parse indicators

--- analyzer/analyzer.py
+++ analyzer/analyzer.py
@@ -6,34 +6,36 @@ from req import Request
 
 def get_page_analyze(page: Page, request: Request) -> dict:
 	output = {
-		# 0
+		# 0 - int
 		'title_to_h1': TextAnalyzer.Correlation.text_text(
 			page.title, page.h1),
-		# 1
+		# 1 - int
 		'title_to_req': TextAnalyzer.Correlation.text_text(
 			page.title, request.text),
-		# 2
+		# 2 - bool
 		'req_in_text': int(TextAnalyzer.text_in_text(
 			request.text, page.text)),
-		# 3
+		# 3 - int
 		'req_words_count': TextAnalyzer.compare_keywords_count(
 			page, request),
-		# 4
+		# 4 - bool
 		'req_in_20text': int(TextAnalyzer.text_in_text(
 			request.text, TextAnalyzer._get_text_percent(page.text, 20))),
-		# 6
+		# 6 - int
 		'keywords_description': TextAnalyzer.Correlation.text_list(
 			page.description, request.words),
-		# 9
+		# 9 - bool
 		'has_list_or_table': int(HTMLAnalyzer.has_tags(
 			page, 'ol', 'ul', 'table')),
-		# 10
+		# 10 - int
 		'req_in_h': TextAnalyzer.request_in_captions(
 			request.text, page.captions),
-		# 11
+		# 11 - bool
 		'imgs_number': int(HTMLAnalyzer.imgs_number_in_text(
 			page.text, page.imgs)),
 	}
+	for k, v in output.items():
+		print(f'{k}: {v}')
 
 	return output
 
