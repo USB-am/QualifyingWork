91451431add13ee83c438bd88e1c76418561d94b
USB-am <smit.acer@mail.ru>
2023-02-12 18:22:33 +0300

    Added matrix generator to HAM

--- analyzer/ham.py
+++ analyzer/ham.py
@@ -12,10 +12,10 @@ class Node:
 		return f'<Node {self.name}>'
 
 
-class Indicator:
-	def __init__(self, name: str, value: Union[int, float]):
+class Indicator(dict):
+	def __init__(self, name: str, **kwargs):
 		self.name = name
-		self.value = value
+		super().__init__(**kwargs)
 
 
 class Matrix:
@@ -29,21 +29,49 @@ class Matrix:
 		~params:
 		: indicator: Indicator - экземпляр класса Indicator
 		'''
-		self._indicators.append(block)
+		self._indicators.append(indicator)
 		self._update_titles()
 
 	def _update_titles(self) -> None:
-		self.titles = list({titles.add(indicator.name) \
-			for indicator in self._indicators})
+		titles = set()
+
+		for indicator in self._indicators:
+			for key in indicator.keys():
+				titles.add(key)
+
+		self.titles = titles
 
 	def get_importance(self) -> list:
-		pass
+		matrix = []
+		
+		for ind in self._indicators:
+			for title in self.titles:
+				matrix.append([])
+
+				for i in self._indicators:
+					value = ind.get(title, 0) / i.get(title, 1)
+					matrix[-1].append(value)
+
+		return matrix
 
 
 def main():
-	n1 = Node('root', 50)
-	n2 = Node('child_1', 45, n1)
-	print(n1, n2, sep='\n')
+	i1 = Indicator(name='Indicator 1', title_h1=80, description_req=75)
+	i2 = Indicator(name='Indicator 2', title_h1=60, description_req=95)
+	i3 = Indicator(name='Indicator 3', title_h1=75, description_req=50)
+	i4 = Indicator(name='Indicator 4', title_h1=95, description_req=40)
+
+	M = Matrix()
+	M.add_indicator(i1)
+	M.add_indicator(i2)
+	M.add_indicator(i3)
+	M.add_indicator(i4)
+
+	for row in M.get_importance():
+		for col in row:
+			print(round(col, 2), end=' ')
+
+		print()
 
 
 if __name__ == '__main__':
