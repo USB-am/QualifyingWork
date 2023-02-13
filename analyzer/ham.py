47457e6ffff30d0f1a97828d9843bd88b5098059
USB-am <smit.acer@mail.ru>
2023-02-13 03:08:40 +0300

    Finished hierarcy analysis method

--- analyzer/ham.py
+++ analyzer/ham.py
@@ -1,120 +1,142 @@
 # hierarchy analysis method
+
 from typing import Union
 
 
-class Indicator(dict):
-	def __init__(self, name: str, **kwargs):
-		self.name = name
-		super().__init__(**kwargs)
+def P(*args) -> Union[float, int]:
+	output = args[0]
+	for number in args[1:]:
+		output *= number
+
+	return output
 
 
-def build_matrix(ind_1: Indicator, ind_2: Indicator) -> list:
-	titles = tuple(ind_1.keys())
-	m = []
+# Информация
+DATA = [
+	[1000, 500,  1000, 1, 0, 2000],
+	[1800, 200,  500,  2, 1, 1000],
+	[800,  1000, 1500, 0, 2, 3000],
+	[2000, 500,  100,  0, 0, 500 ],
+]
 
-	for row in range(len(titles)):
-		m.append([])
-		for col in range(len(titles)):
-			if col == row:
-				value = 1
-			elif col > row:
-				value = ind_1.get(titles[col], 0) / ind_2.get(titles[col], 0)
-			elif col < row:
-				value = ind_2.get(titles[col], 0) / ind_1.get(titles[col], 0)
-			m[-1].append(value)
+# Важность критериев
+CRITERION_IMPORTANCE = [
+	[1,   9,   5,   7,   4,   3  ],
+	[1/9, 1,   1/5, 1/3, 1/6, 1/7],
+	[1/5, 5,   1,   3,   1/2, 1/3],
+	[1/7, 3,   1/3, 1,   1/4, 1/5],
+	[1/4, 6,   2,   4,   1,   1/2],
+	[1/3, 7,   3,   5,   2,   1  ],
+]
+# Цена товара
+CRITERION_PRICE = [
+	[1,   7,   1/2, 8  ],
+	[1/7, 1,   1/8, 2  ],
+	[2,   8,   1,   9  ],
+	[1/8, 1/2, 1/9, 1  ],
+]
+CRITERION_VOLUME = [
+	[1,   4,   1/6, 1  ],
+	[1/4, 1,   1/6, 1/4],
+	[6,   9,   1,   6  ],
+	[1,   4,   1/6, 1  ],
+]
+CRITERION_LOCATION = [
+	[1,   1/5, 3, 1/7],
+	[5,   1,   7, 1/3],
+	[1/3, 1/7, 1, 1/9],
+	[7,   3,   9, 1  ],
+]
+CRITERION_FAILURE = [
+	[1,   5,   1/5, 1/5],
+	[1/5, 1,   1/9, 1/9],
+	[5,   9,   1,   1  ],
+	[5,   9,   1,   1  ],
+]
+CRITERION_TIMES = [
+	[1,   5,   7,   1  ],
+	[1/5, 1,   3,   1/5],
+	[1/7, 1/3, 1,   1/7],
+	[1,   5,   7,   1  ],
+]
+CRITERION_TRANSPORTATION = [
+	[1,   1/5, 3, 1/7],
+	[5,   1,   7, 1/3],
+	[1/3, 1/7, 1, 1/9],
+	[7,   3,   9, 1  ],
+]
 
-	return m
-
-
-def get_max_indicator(indicators: list) -> Indicator:
-	def get_max(ind_1: Indicator, ind_2: Indicator) -> Indicator:
-		matrix = []
-		for title in ind_1.keys():
-			pass
-
-	max_indicator = get_max(indicators[0], indicators[1])
-
-	for indicator in indicators[2:]:
-		max_indicator = get_max(max_indicator, indicator)
-
-	return max_indicator
-
-
-class Matrix:
-	def __init__(self):
-		self._indicators = []
-		self.titles = []
-
-	def add_indicator(self, indicator: Indicator) -> None:
-		'''
-		Добавляет новый показатель
-		~params:
-		: indicator: Indicator - экземпляр класса Indicator
-		'''
-		self._indicators.append(indicator)
-		self._update_titles()
-
-	def _update_titles(self) -> None:
-		titles = set()
-
-		for indicator in self._indicators:
-			for key in indicator.keys():
-				titles.add(key)
-
-		self.titles = titles
-
-	def get_importance(self) -> list:
-		matrix = []
-		
-		for ind in self._indicators:
-			for title in self.titles:
-				matrix.append([])
-
-				for i in self._indicators:
-					value = ind.get(title, 0) / i.get(title, 1)
-					matrix[-1].append(value)
-
-		return matrix
+# Случайная согласованность
+RANDOM_CONSISTENCY_PARAMS = [0, 0, .58, .9, 1.12, 1.24, 1.32, 1.41, 1.45, 1.49]
 
 
-def main():
-	args_1 = {
-		'price': 1000,
-		'volume': 500,
-		'marker': 1000,
-		'errors': 1,
-		'moving': 2000,
-	}
-	args_2 = {
-		'price': 1800,
-		'volume': 200,
-		'marker': 500,
-		'errors': 2,
-		'moving': 1000
-	}
-	i1 = Indicator(name='Indicator 1', **args_1)
-	i2 = Indicator(name='Indicator 2', **args_2)
-	i3 = Indicator(name='Indicator 3', title_h1=75, description_req=50)
-	i4 = Indicator(name='Indicator 4', title_h1=95, description_req=40)
+def get_consistency_attribute(criterion_importance: list,
+	                          local_vectors: list) -> float:
+	'''
+	Расчитывает отношение согласованности
+	~params:
+	: criterion_importance: list - критерии оценки;
+	: local_vectors: list - локальные векторы матрицы
+	'''
+	n = len(criterion_importance[0])
+	# Индекс согласованности (ИС)
+	I_max = sum([sum(col)*local_vectors[i] \
+		for i, col in enumerate(zip(*criterion_importance))])
+	consistency_index = (I_max - n) / (n - 1)
+	# Случайная согласованность
+	random_consistency = RANDOM_CONSISTENCY_PARAMS[n-1]
+	# Отношение согласованности
+	consistency_attribute = consistency_index / random_consistency
 
-	M = Matrix()
-	M.add_indicator(i1)
-	M.add_indicator(i2)
-	M.add_indicator(i3)
-	M.add_indicator(i4)
+	return consistency_attribute
 
-	# for row in M.get_importance():
-	# 	for col in row:
-	# 		print(round(col, 2), end=' ')
 
-	# 	print()
+def get_criterion_vectors(criterion_importance: list) -> float:
+	'''
+	Расчитывает локальный векторы критериев.
+	~params:
+	: criterion_importance: list - критерии оценки
+	'''
+	n = len(criterion_importance[0])
+	# Произведения строк важности
+	criterion_product = [P(*row) for row in criterion_importance]
+	# Корени от произведения строк важности
+	number_root = [product**(1/n) for product in criterion_product]
+	# Локальные векторы
+	local_vectors = [nr/sum(number_root) for nr in number_root]
 
-	for row in build_matrix(i1, i2):
-		for col in row:
-			print(round(col, 2), end=' ')
+	return local_vectors
 
-		print()
+
+def hierarchy_analysis_method():
+	criterion_vectors = get_criterion_vectors(CRITERION_IMPORTANCE)
+	consistency_attribute = get_consistency_attribute(
+		CRITERION_IMPORTANCE, criterion_vectors)
+	# Проверка согласованности
+	cc = consistency_attribute <= .1
+	if not cc:
+		raise ValueError('Некорректные важности критериев!!!')
+
+	price_vectors = get_criterion_vectors(CRITERION_PRICE)
+	volume_vectors = get_criterion_vectors(CRITERION_VOLUME)
+	location_vectors = get_criterion_vectors(CRITERION_LOCATION)
+	failure_vectors = get_criterion_vectors(CRITERION_FAILURE)
+	times_vectors = get_criterion_vectors(CRITERION_TIMES)
+	transportation_vectors = get_criterion_vectors(CRITERION_TRANSPORTATION)
+
+	price_priority = sum([cv*v for cv, v in zip(criterion_vectors, price_vectors)])
+	volume_priority = sum([cv*v for cv, v in zip(criterion_vectors, volume_vectors)])
+	location_priority = sum([cv*v for cv, v in zip(criterion_vectors, location_vectors)])
+	failure_priority = sum([cv*v for cv, v in zip(criterion_vectors, failure_vectors)])
+	times_priority = sum([cv*v for cv, v in zip(criterion_vectors, times_vectors)])
+	transportation_priority = sum([cv*v for cv, v in zip(criterion_vectors, transportation_vectors)])
+
+	max_priority = max(price_priority, volume_priority, location_priority,
+		failure_priority, times_priority, transportation_priority)
+
+	# print(max_priority)
+	return max_priority
 
 
 if __name__ == '__main__':
-	main()
+	print(hierarchy_analysis_method())
